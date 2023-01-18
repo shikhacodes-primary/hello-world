35571cb3bfa43430b520235b4f38b1a937a3f127
Shikha <shikha.shikha@technoidentity.com>
2023-01-18 15:42:34 +0530

    functional component using ES6

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Shikha!</h1>
-}
+//simple functional component
+// function Greet() {
+//     return <h1>Hello Shikha!</h1>
+// }
+
+const Greet = () => <h1>Hello Shikha!</h1>
 
 export default Greet
